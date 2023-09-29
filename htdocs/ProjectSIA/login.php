c12130f0fdb0867c300be7209d8338d8aa89f881
AyelDev <aarielabelgas23@gmail.com>
2023-09-29 22:08:38 +0800

    my first update on php website project

---
+++ htdocs/ProjectSIA/login.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="index.php">
+    <label>Username</label>
+    <input type="text" name="username">
+    </form>
+
+</body>
+
+</html>
